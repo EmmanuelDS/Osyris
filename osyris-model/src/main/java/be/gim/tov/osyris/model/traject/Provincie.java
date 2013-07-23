26bcacf56352080057b5515fa4ffd7d01ab8d62c
kristof <kristof@gim.be>
2013-07-23 12:15:51 +0200

    Added Provincie modelClass

---
+++ osyris-model/src/main/java/be/gim/tov/osyris/model/traject/Provincie.java
@@ -0,0 +1,51 @@
+package be.gim.tov.osyris.model.traject;
+
+import org.conscientia.api.model.StorableObject;
+import org.conscientia.api.model.annotation.Label;
+import org.conscientia.api.model.annotation.Model;
+import org.conscientia.api.model.annotation.ModelStore;
+import org.conscientia.api.model.annotation.NotEditable;
+import org.conscientia.api.model.annotation.NotSearchable;
+import org.conscientia.api.model.annotation.NotViewable;
+import org.conscientia.api.model.annotation.SrsName;
+import org.conscientia.core.model.AbstractModelObject;
+
+import com.vividsolutions.jts.geom.Geometry;
+
+/**
+ * 
+ * @author kristof
+ * 
+ */
+@Model
+@ModelStore("OsyrisDataStore")
+@Label("Provincie")
+public class Provincie extends AbstractModelObject implements StorableObject {
+
+	// VARIABLES
+	@Label("Naam")
+	private String naam;
+
+	@NotSearchable
+	@NotViewable
+	@NotEditable
+	@SrsName("EPSG:31370")
+	private Geometry geom;
+
+	// GETTERS AND SETTERS
+	public String getNaam() {
+		return naam;
+	}
+
+	public void setNaam(String naam) {
+		this.naam = naam;
+	}
+
+	public Geometry getGeom() {
+		return geom;
+	}
+
+	public void setGeom(Geometry geom) {
+		this.geom = geom;
+	}
+}
